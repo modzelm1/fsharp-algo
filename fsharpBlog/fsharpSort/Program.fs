a5af2ad8eb6d7f055a7c7081368e8879d4669eeb
Mike <mike.modzelewski@gmail.com>
2015-06-29 11:44:37 +0200

    sorting

--- fsharpBlog/fsharpSort/Program.fs
+++ fsharpBlog/fsharpSort/Program.fs
@@ -1,12 +1,11 @@
-﻿let rec selectsort = function
+﻿open System
+
+let rec selectsort = function
    | [] -> []
    | inputList ->
         let minVal,greaterThanMinVal = List.partition ((>=) (List.min inputList)) inputList 
         List.concat [minVal; selectsort greaterThanMinVal]
 
-//we don't know if the first element is on the right place
-//so we have to check it every time
-//we increase left list, decreased right list and put element from the right ot left
 let rec insertsortHelper = function
     | ([],[]) -> []
     | (l,[]) -> l
@@ -17,30 +16,60 @@ let rec insertsortHelper = function
         let s, g = List.partition ((>=) h) l
         insertsortHelper (List.concat [s;[h];g], t)
 
-let rec insertsort2 = function
+let rec insertsort = function
    | [] -> []
    | [f] -> [f]
    | f::t ->
         insertsortHelper ([f] , t)
 
-let rec insertsort = function
-   | [] -> []
-   | [first] -> [first]
-   | first::second::tail ->
-        let test = first - second
-        match test with
-        | t when t <= 0 ->
-            insertsortHelper (List.concat[ [first]; [second] ] , tail)
-        | t when t > 0 ->
-            insertsortHelper (List.concat[ [second]; [first] ] , tail)
-        | _ -> []
+//let rec insertsort = function
+//   | [] -> []
+//   | [first] -> [first]
+//   | first::second::tail ->
+//        let test = first - second
+//        match test with
+//        | t when t <= 0 ->
+//            insertsortHelper (List.concat[ [first]; [second] ] , tail)
+//        | t when t > 0 ->
+//            insertsortHelper (List.concat[ [second]; [first] ] , tail)
+//        | _ -> []
 
+
+let rec quicksortFirst = function
+   | [] -> []                         
+   | first::rest -> 
+        let smaller,larger = List.partition ((>=) first) rest 
+        List.concat [quicksortFirst smaller; [first]; quicksortFirst larger]
+
+let rec quicksortLast = function
+   | [] -> []                         
+   | inputList -> 
+        let revertedList = List.rev inputList 
+        quicksortFirst (List.concat [[revertedList.Head]; List.rev revertedList.Tail])
+
+//stolen from: http://stackoverflow.com/questions/2889961/f-insert-remove-item-from-list
+let removeAt index input =
+  input 
+  // Associate each element with a boolean flag specifying whether 
+  // we want to keep the element in the resulting list
+  |> List.mapi (fun i el -> (i <> index, el)) 
+  // Remove elements for which the flag is 'false' and drop the flags
+  |> List.filter fst |> List.map snd
+
+let rec quicksortRandom = function
+   | [] -> []                         
+   | inputList ->
+        let rnd = Random() 
+        let randomIndex = rnd.Next(inputList.Length)
+        let ravdomVal = List.nth inputList randomIndex
+        let listWithoutRandomVal = removeAt randomIndex inputList
+        quicksortFirst (List.concat [[ravdomVal]; listWithoutRandomVal])
 
 [<EntryPoint>]
 let main argv = 
-    //printfn "%A" (quicksort [1;5;23;18;9;1;3])
+    printfn "%A" (quicksortFirst [1;5;23;18;9;1;3])
+    printfn "%A" (quicksortLast [1;5;23;18;9;1;3])
+    printfn "%A" (quicksortRandom [1;5;23;18;9;1;3])
     printfn "%A" (selectsort [1;5;23;18;9;1;3])
     printfn "%A" (insertsort [1;5;23;18;9;1;3])
-    printfn "%A" (insertsort2 [1;5;23;18;9;1;3])
-    //testmin [1;5;23;18;9;1;3]
     0 // return an integer exit code
