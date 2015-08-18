3a63cec161979bc6acc93978b82eeebd93577346
Mike <mike.modzelewski@gmail.com>
2015-08-18 16:05:11 +0200

    suffle

---
+++ fsharpBlog/fsharpRandom/Program.fs
@@ -0,0 +1,31 @@
+﻿open System
+
+let suffle (inputList: int List) = 
+        let rnd = System.Random DateTime.Now.Millisecond
+        List.fold (fun acc item -> 
+                        let randomCardIndex = rnd.Next (fst acc, inputList.Length)
+                        (fst acc) + 1 ,
+                        List.mapi (fun tmpIndex tmpItem -> 
+                            match tmpIndex with 
+                                | t when t = randomCardIndex -> (snd acc).[fst acc]
+                                | t when t = (fst acc) -> (snd acc).[randomCardIndex]
+                                | _ -> tmpItem ) (snd acc) 
+                     ) 
+                     (0, inputList) 
+                     inputList
+
+
+
+[<EntryPoint>]
+let main argv = 
+
+    let toShuffle1 = [1; 2; 3; 4; 5; 6; 7; 8; 9 ]
+    let toShuffle2 = [1; 2; 3; 4; 5; 6; 7; 8; 9 ]
+    let toShuffle3 = [1; 2; 3; 4; 5; 6; 7; 8; 9 ]
+
+    printfn "%A" (suffle toShuffle1)
+    printfn "%A" (suffle toShuffle2)
+    printfn "%A" (suffle toShuffle3)
+
+    0 // return an integer exit code
+    
