cd181b171038d6f6b3b2c7813c8cb18c6af0620e
Mike <mike.modzelewski@gmail.com>
2015-08-25 13:01:22 +0200

    suffleArray

--- fsharpBlog/fsharpRandom/Program.fs
+++ fsharpBlog/fsharpRandom/Program.fs
@@ -14,16 +14,27 @@ let suffleList (inputList: int List) =
                      (0, inputList) 
                      inputList)
 
+//let suffleArrayTest (inputArray: int[]) = 
+//        let rnd = System.Random DateTime.Now.Millisecond
+//        snd (Array.fold (fun acc item -> 
+//                            let randomCardIndex = rnd.Next (fst acc, inputArray.Length)
+//                            let tmp = inputArray.[randomCardIndex]
+//                            inputArray.[randomCardIndex] <- inputArray.[fst acc]
+//                            inputArray.[fst acc] <- tmp
+//                            ((fst acc) + 1 , inputArray)
+//                        ) 
+//                     (0, inputArray) inputArray)
+
 let suffleArray (inputArray: int[]) = 
         let rnd = System.Random DateTime.Now.Millisecond
-        snd (Array.fold (fun acc item -> 
-                            let randomCardIndex = rnd.Next (fst acc, inputArray.Length)
+        inputArray |> Array.fold (fun acc item -> 
+                            let randomCardIndex = rnd.Next (acc, inputArray.Length)
                             let tmp = inputArray.[randomCardIndex]
-                            inputArray.[randomCardIndex] <- inputArray.[fst acc]
-                            inputArray.[fst acc] <- tmp
-                            ((fst acc) + 1 , inputArray)
-                        ) 
-                     (0, inputArray) inputArray)
+                            inputArray.[randomCardIndex] <- inputArray.[acc]
+                            inputArray.[acc] <- tmp
+                            acc + 1
+                        ) 0 
+         |> ignore
 
 let add resultSpace a b =
     let r = new Random(DateTime.Now.Millisecond)
@@ -52,6 +63,7 @@ let main argv =
 
     let toShuffleList = [1..10]
     let toShuffleArray = [|1..10|]
+    let toShuffleArray2 = [|1..10|]
 
     printfn "input:  %A" (toShuffleList)
     printfn "result: %A" (suffleList toShuffleList)
@@ -59,7 +71,9 @@ let main argv =
     printfn ""
 
     printfn "input:  %A" (toShuffleArray)
-    printfn "result: %A" (suffleArray toShuffleArray)
+    suffleArray toShuffleArray
+    printfn "result: %A" (toShuffleArray)
+
 
 //    let loopList = [1..1000]
 //    let result = List.reduce (fun acc elem -> acc + (add 200 36 22)) loopList
